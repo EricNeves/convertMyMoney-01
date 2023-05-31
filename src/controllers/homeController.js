cba01622cd3ff1610df53690d71ad389c4077c66
Eric Neves <ericnevesrodrigues@gmail.com>
2023-05-31 09:45:00 -0400

    Render quotation

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,8 @@
-const index = function(req, res) {
-  res.render('home')
+const { extractQuotation } = require('../lib/api.quotation')
+
+const index = async function(req, res) {
+  const quotation = await extractQuotation()
+  res.render('home', { quotation })
 }
 
 module.exports = { index }
